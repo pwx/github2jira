c8f2397a1caa0335c1af066f9d0d8b1fc557fa0a
Vamsee Kanakala <vkanakala@gmail.com>
2012-05-01 19:32:11 +0530

    pull each issue individually to avoid paging/status complications

--- export_issues.rb
+++ export_issues.rb
@@ -12,6 +12,68 @@ require 'date'
 SKIP_LIST = [639, 542]
 
 module ExportIssues
+  def ExportIssues.get_all_issues(total_issues)
+    issues = []
+    issues_n_comments = []
+    max_comments = 0
+    
+    # pull the issues: make sure you stay below GitHub's (current, generous)
+    # rate limit of 5000 requests per hour.
+
+    1.upto(total_issues).each do |inum|
+      uri = URI.parse("https://api.github.com/repos/pwx/code/issues/#{inum}")
+      http = Net::HTTP.new uri.host, uri.port
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+      
+      http.start { |http|
+        req = Net::HTTP::Get.new(uri.request_uri)
+        req.basic_auth 'vamsee', 'v4m533K4NGH'
+        response = http.request(req)
+        issues[inum-1] = JSON.parse(response.body)
+      }
+      
+      puts "Retrieved issue #{inum}"
+    end
+
+    puts "Retrieving comments for issues"
+
+    issues.each do |issue|
+      issue_id = issue['number']
+      next if SKIP_LIST.include?(issue_id)
+      comments = []
+      summary = issue['title']
+      reporter = get_username(issue['user']['login'])
+      desc = issue['body']
+
+      if desc.match(/\*\*Author:\s.+\*\*/)
+        uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
+        if uname
+          reporter = uname
+          desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
+        end
+      end
+
+      date_created = DateTime.parse(issue['created_at']).strftime("%d/%m/%Y %H:%M:%S")
+      date_updated = DateTime.parse(issue['updated_at']).strftime("%d/%m/%Y %H:%M:%S")
+      status = issue['state']
+      assignee = get_username(issue['assignee']['login']) if issue['assignee']
+      version = issue['milestone']['title'] if issue['milestone']
+
+      if issue['comments'] > 0          
+        comments = get_issue_comments(issue_id, issue['comments'])
+        max_comments = comments.size if comments.size > max_comments
+      else
+        puts "0 comments retrieved in issue #{issue_id}"
+      end
+        
+      issues_n_comments << [summary, desc, date_created, date_updated, 
+                            status, reporter, assignee, version] + comments
+    end
+
+    generate_csv(issues_n_comments, max_comments)
+  end
+
   def ExportIssues.get_issues(total_issues, per_page=100)
     issue_pages = []
     issues = []
@@ -50,10 +112,8 @@ module ExportIssues
 
         if desc.match(/\*\*Author:\s.+\*\*/)
           uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
-          if uname
-            reporter = uname
-            desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
-          end
+          reporter = uname if uname
+          desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
         end
 
         date_created = DateTime.parse(issue['created_at']).strftime("%d/%m/%Y %H:%M:%S")
@@ -113,7 +173,15 @@ module ExportIssues
       cp.each do |comment|
         username = get_username(comment['user']['login'])
         date = DateTime.parse(comment['created_at']).strftime("%d/%m/%y %H:%M:%S")
-        comments << ["#{date}; #{username}; #{comment['body']}"]
+        desc = comment['body']
+        
+        if desc.match(/\*\*Author:\s.+\*\*/)
+          uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
+          username = uname if uname
+          desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
+        end
+        
+        comments << ["#{date}; #{username}; #{desc}"]
       end
     end
 
