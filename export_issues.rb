4020721babc748241b20926b263180ef177fbcf9
Vamsee Kanakala <vkanakala@gmail.com>
2012-05-03 15:15:10 +0530

    add fixversion too so that issues get properly attached to versions

--- export_issues.rb
+++ export_issues.rb
@@ -7,10 +7,6 @@ require 'json'
 require 'fastercsv'
 require 'date'
 
-# add the issues that are causing problems with import
-# we can fix these later
-SKIP_LIST = []
-
 module ExportIssues
   def ExportIssues.get_all_issues(issue_start, issue_end)
     issues = []
@@ -28,7 +24,7 @@ module ExportIssues
       
       http.start { |http|
         req = Net::HTTP::Get.new(uri.request_uri)
-        req.basic_auth 'vamsee', 'v4m533K4NGH'
+        req.basic_auth 'githubusername', 'githubpassword'
         response = http.request(req)
         issues[idx] = JSON.parse(response.body)
       }
@@ -45,12 +41,6 @@ module ExportIssues
       reporter = get_username(issue['user']['login'])
       desc = issue['body'] || 'No description given'
 
-      if SKIP_LIST.include?(issue_id)
-        # issue is giving problems either in description or in comments,
-        # so setup only title and do the rest manually (including comments):
-        desc = "Skipped migration due to csv probs. Please refer https://github.com/pwx/code/issues/#{issue_id}"
-      end
-
       if desc.match(/\*\*Author:\s.+\*\*/)
         uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
         if uname
@@ -66,80 +56,17 @@ module ExportIssues
       assignee = get_username(issue['assignee']['login']) if issue['assignee']
       version = issue['milestone']['title'] if issue['milestone']
 
-      if issue['comments'] > 0 && !SKIP_LIST.include?(issue_id)
+      if issue['comments'] > 0
         comments = get_issue_comments(issue_id, issue['comments'])
         max_comments = comments.size if comments.size > max_comments
       else
         puts "0 comments retrieved in issue #{issue_id}"
       end
         
-      issues_n_comments << [summary, desc, date_created, date_updated, status, reporter, assignee, version, resolution] + comments
+      issues_n_comments << [summary, desc, date_created, date_updated, status, reporter, assignee, version, version, resolution] + comments
     end
 
     generate_csv(issues_n_comments, max_comments)
-  end
-
-  def ExportIssues.get_issues(total_issues, per_page=100)
-    issue_pages = []
-    issues = []
-    max_comments = 0
-    
-    total_pages = total_issues/per_page
-    total_pages += 1 if total_issues % per_page > 0
-
-    # pull the issues: make sure you stay below GitHub's (current, generous)
-    # rate limit of 5000 requests per hour.
-
-    1.upto(total_pages).each do |page|
-      uri = URI.parse("https://api.github.com/repos/pwx/code/issues?state=closed&page=#{page}&per_page=#{per_page}")
-      http = Net::HTTP.new uri.host, uri.port
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      http.use_ssl = true
-
-      http.start { |http|
-        req = Net::HTTP::Get.new(uri.request_uri)
-        req.basic_auth 'vamsee', 'v4m533K4NGH'
-        response = http.request(req)
-        issue_pages[page-1] = JSON.parse(response.body)
-      }
-
-      puts "#{issue_pages[page-1].size} issues retrieved in page #{page}"
-    end
-
-    issue_pages.each do |ip|
-      ip.each do |issue|
-        issue_id = issue['number']
-        next if SKIP_LIST.include?(issue_id)
-        comments = []
-        summary = issue['title']
-        reporter = get_username(issue['user']['login'])
-        desc = issue['body']
-
-        if desc.match(/\*\*Author:\s.+\*\*/)
-          uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
-          reporter = uname if uname
-          desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
-        end
-
-        date_created = DateTime.parse(issue['created_at']).strftime("%d/%m/%Y %H:%M:%S")
-        date_updated = DateTime.parse(issue['updated_at']).strftime("%d/%m/%Y %H:%M:%S")
-        status = issue['state']
-        assignee = get_username(issue['assignee']['login']) if issue['assignee']
-        version = issue['milestone']['title'] if issue['milestone']
-
-        if issue['comments'] > 0          
-          comments = get_issue_comments(issue_id, issue['comments'])
-          max_comments = comments.size if comments.size > max_comments
-        else
-          puts "0 comments retrieved in issue #{issue_id}"
-        end
-        
-        issues << [summary, desc, date_created, date_updated, 
-                   status, reporter, assignee, version] + comments
-      end
-    end
-
-    generate_csv(issues.reverse, max_comments)
   end
 
   def ExportIssues.get_username(name)
@@ -182,8 +109,10 @@ module ExportIssues
         
         if desc.match(/\*\*Author:\s.+\*\*/)
           uname = get_username(desc.match(/\*\*Author:\s(.+)\*\*/)[1])
-          username = uname if uname
-          desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
+          if uname
+            username = uname 
+            desc = desc.gsub(/\*\*Author:\s.+\*\*/,'')
+          end
         end
         
         comments << ["#{date}; #{username}; #{desc}"]
@@ -196,7 +125,7 @@ module ExportIssues
   def ExportIssues.generate_csv(issues, max_comments)
     FasterCSV.open('ghissues.csv', 'w') do |csv|   
       comment_cols = ['CommentBody'] * max_comments
-      csv << ['Summary', 'Description', 'DateCreated', 'DateModified', 'Status', 'Reporter', 'Assignee', 'AffectsVersion', 'Resolution'] + comment_cols
+      csv << ['Summary', 'Description', 'DateCreated', 'DateModified', 'Status', 'Reporter', 'Assignee', 'AffectsVersion', 'FixVersion', 'Resolution'] + comment_cols
       issues.each do |issue|
         csv << issue
       end
